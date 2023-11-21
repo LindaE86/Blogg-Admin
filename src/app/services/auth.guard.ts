c62fa309fd02d3321845687aed5bcd15c917775b
LindaE86 <linda.malmborg@hotmail.se>
2023-11-21 09:39:44 +0100

    finish

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,20 +1,16 @@
-
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { ToastrService } from 'ngx-toastr';
 
-  Injectable({
+  @Injectable({
     providedIn: 'root'
   })
 
-export class AuthGuard {
+export class AuthGuard implements CanActivate {
 
-  constructor( 
-    private authService: AuthService, 
-    private router: Router, 
-    private toaster: ToastrService ) {}
+  constructor( private authService: AuthService, private router: Router, private toaster: ToastrService ) {}
 
  canActivate(
    route: ActivatedRouteSnapshot,
@@ -24,7 +20,7 @@ export class AuthGuard {
     return true;
     }
     else {
-      this.toaster.warning('You dont have permission to acces this page');
+      this.toaster.warning('You dont have permission to acces this page')
       this.router.navigate(['/login']);
       return false;
     }
